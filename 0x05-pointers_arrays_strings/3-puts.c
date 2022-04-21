cf876dd072d8f590766714bc2569d9f58cbc94aa
Peace-Aielumoh <peaceaielumoh@gmail.com>
2022-04-21 06:46:50 -0700

    A function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a spring
+ * @str:A pointer to an int that will be changed
+ * 
+ * Return: void which means our answer is correct
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
+return;
+}
